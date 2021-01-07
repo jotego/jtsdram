9ec2d3d2f70dd02fa6cb83c41bae2e0be7022b4b
jotego <jotego@gmail.com>
2021-01-07 19:20:14 +0100

    wip

--- hdl/jtsdram_bank_ro.v
+++ hdl/jtsdram_bank_ro.v
@@ -93,6 +93,7 @@ always @(posedge clk, posedge rst) begin
                 cnt_addr <= cnt_addr + 1'd1;
                 if( dout !== data_ref ) bad <= 1;
             end
+            else if(!dly_cs && !cs) cs <= 1;
         end
     end
 end
